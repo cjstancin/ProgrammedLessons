bd675d81ae22754ab2290e71e5fc020db0bf4eab
cjstancin <cjstancin@outlook.com>
2020-09-10 10:57:22 -0600

    Finished Rainfall Exercise

---
+++ Chapter10/Rainfall.java
@@ -0,0 +1,12 @@
+public class Rainfall {
+    public static void main(final String[] args){
+        double april = 12;
+        double may = 14;
+        double june = 8;
+        double avg = (april+may+june)/3;
+        System.out.println("Rainfall for April:  " + april);
+        System.out.println("Rainfall for May:  " + may);
+        System.out.println("Rainfall for June:  " + june);
+        System.out.println("The Average Rainfall was: " + avg);
+    }
+}
