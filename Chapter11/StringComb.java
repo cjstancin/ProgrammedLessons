34b16d3ca2ca8cffabce09cdc5871908974bd179
cjstancin <cjstancin@outlook.com>
2020-09-15 09:25:37 -0600

    Chapter 11 3 Exercises completed

---
+++ Chapter11/StringComb.java
@@ -0,0 +1,8 @@
+public class StringComb {
+    public static void main(String[] args) {
+        String first = "    In a Hole   in the ground there lived a Hobbit. ";
+        String second;
+        second = first.trim();
+        System.out.println(second);
+    }
+}
---
+++ Chapter11/StringTest.java
@@ -0,0 +1,9 @@
+public class StringTest {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        String secondVariable = "";
+        System.out.println(str);
+        System.out.println(secondVariable);
+
+    }
+}
