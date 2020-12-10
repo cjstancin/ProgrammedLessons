0f631338b95a409e5ab8380f83f00140b22f8afa
cjstancin <cjstancin@outlook.com>
2020-12-10 14:31:51 -0700

    Fixed Things

--- Chapter54/Combinations.java
+++ Chapter54/Combinations.java
@@ -2,6 +2,15 @@ import java.util.Scanner;
 
 public class Combinations {
 
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter N");
+        int n = scan.nextInt();
+        System.out.println("Enter R");
+        int r = scan.nextInt();
+        System.out.println("Combination: " + nChooseR(n, r));
+    }
+    
     public static long factorial(int num) {
         long factorial = 1;
         for (int j = 1; j <= num; j++){
@@ -12,13 +21,5 @@ public class Combinations {
 
     public static long nChooseR(int n, int r) {
         return (factorial(n) / (factorial(r)*factorial(n - r)));
-    }
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter N");
-        int n = scan.nextInt();
-        System.out.println("Enter R");
-        int r = scan.nextInt();
-        System.out.println("N choose R: " + nChooseR(n, r));
     }
 }
